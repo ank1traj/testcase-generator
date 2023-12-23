d4d168cc0e08544778f6403f796c4aaef1cf447d
singhcoder694 <vivekkr694@gmail.com>
2023-12-23 10:35:08 +0530

    Resolved Converstation

--- pages/sign-in/[[...index]].jsx
+++ pages/sign-in/[[...index]].jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { SignIn } from '@clerk/nextjs'
+import styles from "@/styles/Home.module.css";
 export default function SignInPage() {
   return (
     <div>
-        <div style={{display:"flex", justifyContent:"center",height:"100vh",alignItems:"center"}}>
+        <div className={styles.signContainer}>
             <SignIn />
         </div>
     </div>
--- pages/sign-up/[[...index]].jsx
+++ pages/sign-up/[[...index]].jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { SignUp } from '@clerk/nextjs'
+import styles from "@/styles/Home.module.css";
 export default function SignUpPage() {
   return (
     <div>
-        <div style={{display:"flex", justifyContent:"center",height:"100vh",alignItems:"center"}}>
+        <div className={styles.signContainer}>
             <SignUp />
         </div>
     </div>
