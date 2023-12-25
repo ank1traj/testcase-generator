51b601e8bb942a66bee5405e6d131c7794a835c0
singhcoder694 <vivekkr694@gmail.com>
2023-12-25 16:28:59 +0530

    Removed Unnecessary

--- pages/api/confirm.js
+++ pages/api/confirm.js
@@ -1,35 +1,28 @@
 import { transporter } from "@/config/nodemailer";
-import toast from "react-hot-toast";
 
 export default async function handler(req, res) {
     const data=req.body;
-    try{
-        if (data.value=="Test Case Generated Successfully!!!"){
-            await transporter.sendMail({
-                from: data.email,
-                to:process.env.EMAIL,
-                subject: data.value,
-                html: "<strong>Test Cases has been generated successfully.</strong>",
-                attachments:[
-                    {
-                        filename: data.filename,
-                        content: data.content,
-                        contentType: "text/plain"
-                    }
-                ]
-            })
-        }
-        else{
-            await transporter.sendMail({
-                from: data.email,
-                to:process.env.EMAIL,
-                subject: data.value,
-                html: "<p>Couldn't Generate Test Cases due to some Error.",
-            })
-        }
+    if (data.value=="Test Case Generated Successfully!!!"){
+        await transporter.sendMail({
+            from: data.email,
+            to:process.env.EMAIL,
+            subject: data.value,
+            html: "<strong>Test Cases has been generated successfully.</strong>",
+            attachments:[
+                {
+                    filename: data.filename,
+                    content: data.content,
+                    contentType: "text/plain"
+                }
+            ]
+        })
     }
-    catch(error){
-        toast.error(error);
+    else{
+        await transporter.sendMail({
+            from: data.email,
+            to:process.env.EMAIL,
+            subject: data.value,
+            html: "<p>Couldn't Generate Test Cases due to some Error.",
+        })
     }
-    res.status(200).json({ name: 'John Doe' })
 }
